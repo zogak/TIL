f54576d33e8bf9930507a93aefcb5dd6b3d5cbe7
zogak <zogak7@gmail.com>
2021-08-16 00:53:21 +0900

    8/16 chapter5

--- flask/goalToComplete/forms.py
+++ flask/goalToComplete/forms.py
@@ -1,3 +1,4 @@
+from models import Fcuser
 from flask_wtf import FlaskForm
 from wtforms import StringField
 from wtforms import PasswordField
@@ -8,3 +9,20 @@ class RegisterForm(FlaskForm):
     username = StringField('username', validators=[DataRequired()])
     password = PasswordField('password', validators=[DataRequired(), EqualTo('repassword')])
     repassword = PasswordField('repassword', validators=[DataRequired()])
+
+class LoginForm(FlaskForm):
+
+    class UserPassword(object):
+        def __init__(self, message=None):
+            self.message = message
+
+        def __call__(self, form, field):
+            userid = form['userid'].data
+            password = field.data
+
+            fcuser = Fcuser.query.filter_by(userid=userid).first()
+            if fcuser.password != password:
+                raise ValueError('wrong password')
+
+    userid = StringField('userid', validators=[DataRequired()])
+    password = PasswordField('password', validators=[DataRequired(), UserPassword()])
